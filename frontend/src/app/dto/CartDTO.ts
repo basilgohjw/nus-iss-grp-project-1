74545abcf975974bc43913252e319f815c330765
Sean Leng <seanleng.tan@ncs.com.sg>
2023-10-18 23:56:43 +0800

    remove frontend and backend

--- frontend/src/app/dto/CartDTO.ts
+++
@@ -1,4 +0,0 @@
-export class CartDTO {
-    cartId: number;
-    cartQuantity: number;
-}
--- frontend/src/app/dto/ProductDTO.ts
+++
@@ -1,6 +0,0 @@
-export class ProductDTO {
-    productId: number;
-    productName: string;
-    productPrice: number;
-    productQuantity: number;
-}
