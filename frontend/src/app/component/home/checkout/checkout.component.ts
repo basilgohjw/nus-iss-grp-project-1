aee8875d7aa1d0f9ddfe3583ddae12f825dbe86d
wong chuan kai <wongchuankai97@gmail.com>
2023-10-24 22:58:34 +0800

    [frontend] checkout page. change payment type to select option and amount is retrieved from cart. phonenumber is retrieved from address.

--- frontend/src/app/component/home/checkout/checkout.component.ts
+++ frontend/src/app/component/home/checkout/checkout.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Payment } from 'src/app/model/payment';
 import { ApiService } from 'src/app/service/api.service';
 import { Router } from '@angular/router';
+import { Address } from 'src/app/model/address';
+import { Cart } from 'src/app/model/cart';
 
 @Component({
   selector: 'app-checkout',
@@ -10,12 +12,39 @@ import { Router } from '@angular/router';
 })
 export class CheckoutComponent implements OnInit {
 
+  cartlist: Cart[] = [];
+  totalSum: number = 0;
   private paymentForm: any;
   fileToUpload: File = null;
   imageUrl: string = "/assets/img/noimage.png";
+  
+  model: Address = {
+    address: '',
+    city: '',
+    state: '',
+    country: '',
+    zipcode: '',
+    phonenumber: ''
+
+  };
+
   constructor(private api: ApiService, private route: Router) { }
 
   ngOnInit() {
+    this.api.getCartItems().subscribe(res => {
+      this.cartlist = res.oblist;
+      this.cartlist.forEach(value => {
+        this.totalSum = this.totalSum + (value.quantity * value.price);
+      });
+    });
+
+    this.api.getAddress().subscribe(res => {
+      if (res.map != null) {
+        this.model = res.map;
+      }
+    }, err => {
+      console.log(err);
+    });
   }
 
   handleFileInput(file: FileList) {
@@ -36,14 +65,19 @@ export class CheckoutComponent implements OnInit {
   placeOrder(paymentType: any, amount: any, phone: any, image:any) {
     console.log("paymentType: ", paymentType.value);
     console.log("amount", amount.value);
+    console.log("phone", phone.value);
     if (paymentType == undefined || paymentType == null || paymentType == '' || amount == undefined || amount == null || amount == 0) {
       alert("Please fill in the mandatory fields.");
     } else {
       this.api.placeOrder().subscribe(res => {
         console.log(res);
         console.log("orderid:: ",res.oblist[0].orderId);
-        this.api.processOrder(paymentType.value, amount.value, phone.value = (amount.value === undefined) ? 0:amount.value, this.fileToUpload, res.oblist[0].orderId).subscribe(data => {
+        this.api.processOrder(paymentType.value, amount.value, phone.value = (phone.value === undefined) ? 0:phone.value, this.fileToUpload, res.oblist[0].orderId).subscribe(data => {
           console.log("process order:: ", data);
+          this.api.getCartItems().subscribe(res2 => {
+            this.cartlist = res2.oblist;
+            this.api.cartAmount = this.cartlist.length;
+          });
         })
       });
       this.route.navigate(['/home']);
