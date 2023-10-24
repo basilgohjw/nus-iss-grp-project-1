477a48632767d1f0525ce32a1c36734df5f0da29
wong chuan kai <wongchuankai97@gmail.com>
2023-10-24 21:57:38 +0800

    [frontend] Added price tag in product card and added santizer to enable image to be shown in card.

--- frontend/src/app/component/home/product/product.component.ts
+++ frontend/src/app/component/home/product/product.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from 'src/app/model/product';
 import { EventEmitter } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -12,17 +12,26 @@ import { EventEmitter } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  imageSource: any;
   @Input() public product;
 
   @Output() productAddToCart: EventEmitter<Product> = new EventEmitter<Product>();
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-
+    this.convertBase64ToImage(this.product.productimage);
   }
 
   addToCart() {
     this.productAddToCart.emit(this.product);
   }
 
+  convertBase64ToImage(base64Data: string) {
+    // Sanitize the Base64 data
+    const sanitizedData = this.sanitizer.bypassSecurityTrustUrl(`data:image/jpeg;base64,${base64Data}`);
+
+    // Assign the sanitized data to the image source
+    this.imageSource = sanitizedData;
+  }
 }
