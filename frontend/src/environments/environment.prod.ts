df9c69b926f4a81a14d6988cb986025f361ffe34
wong chuan kai <wongchuankai97@gmail.com>
2023-10-25 15:20:58 +0800

    no message

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // user base url
   userBaseUrl:"http://nus-iss-grp1-backend-user-alb-186458001.ap-southeast-1.elb.amazonaws.com/user",
   // product base url
-  productBaseUrl:"nus-iss-grp1-product-alb-1028734771.ap-southeast-1.elb.amazonaws.com/product",
+  productBaseUrl:"http://nus-iss-grp1-product-alb-1028734771.ap-southeast-1.elb.amazonaws.com/product",
   // order cart base url
-  orderBaseUrl:"nus-iss-grp1-backend-cart-alb-1700263701.ap-southeast-1.elb.amazonaws.com/cart"
+  orderBaseUrl:"http://nus-iss-grp1-backend-cart-alb-1700263701.ap-southeast-1.elb.amazonaws.com/cart"
 };
