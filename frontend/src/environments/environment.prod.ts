894a0287edf04f94f8f56aee56319a9a4d83f7ce
wong chuan kai <wongchuankai97@gmail.com>
2023-10-25 14:30:52 +0800

    no message

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // user base url
-  userBaseUrl:"http://localhost:80821",
+  userBaseUrl:"http://nus-iss-grp1-backend-user-alb-186458001.ap-southeast-1.elb.amazonaws.com/user",
   // product base url
   productBaseUrl:"http://localhost:80831",
   // order cart base url
