e5b82da4dcd9640cb8f55eaf56cb7b1d79774876
wong chuan kai <wongchuankai97@gmail.com>
2023-10-25 15:16:53 +0800

    [frontend] add api for prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // user base url
   userBaseUrl:"http://nus-iss-grp1-backend-user-alb-186458001.ap-southeast-1.elb.amazonaws.com/user",
   // product base url
-  productBaseUrl:"http://localhost:80831",
+  productBaseUrl:"nus-iss-grp1-product-alb-1028734771.ap-southeast-1.elb.amazonaws.com/product",
   // order cart base url
-  orderBaseUrl:"http://localhost:80841"
+  orderBaseUrl:"nus-iss-grp1-backend-cart-alb-1700263701.ap-southeast-1.elb.amazonaws.com/cart"
 };
